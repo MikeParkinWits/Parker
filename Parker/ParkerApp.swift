97857ef0c0a341061b87f0f98e4e8d0753191605
Michael Parkin <1894979@students.wits.ac.za>
2022-10-04 18:49:23 +0200

    Initial Commit

---
+++ Parker/ParkerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParkerApp.swift
+//  Parker
+//
+//  Created by Mike Parkin on 2022/10/04.
+//
+
+import SwiftUI
+
+@main
+struct ParkerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
